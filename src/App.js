4b80d537527875ba823d69751e086f09165bb1db
mdons <michaeljdonnell@gmail.com>
2019-01-08 20:48:23 -0600

    end of class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="right">
+          <p>Right Answer</p>
+        </div>
+        <div className="wrong">
+          <p>Wrong Answer</p>
+        </div>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((num, i) => {})}
       </div>
     );
   }
