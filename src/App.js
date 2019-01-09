6025ccb2cd805ea00169c071268883fadc280db9
mdons <michaeljdonnell@gmail.com>
2019-01-08 22:22:59 -0600

    finished number guesser

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rightNumber: 5,
+      correct: false
+    };
+  }
+
+  guessNumber(num) {
+    this.setState({ correct: this.state.rightNumber === num });
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="right">
-          <p>Right Answer</p>
-        </div>
-        <div className="wrong">
-          <p>Wrong Answer</p>
-        </div>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((num, i) => {})}
+        {this.state.correct && (
+          <div className="right">
+            <p>Right Answer</p>
+          </div>
+        )}
+        {!this.state.correct && (
+          <div className="wrong">
+            <p>Wrong Answer</p>
+          </div>
+        )}
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((num, i) => {
+          return (
+            <div
+              key={i}
+              onClick={() => this.guessNumber(num)}
+              className="num-box"
+            >
+              <p>{num}</p>
+            </div>
+          );
+        })}
       </div>
     );
   }
